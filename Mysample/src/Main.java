c8fcb32ad77df5854846d1ac4fe6d2c7f9ddab07
srenutechrepo <44047683+srenutechrepo@users.noreply.github.com>
2018-11-11 00:10:48 +0530

    server second changes

--- Mysample/src/Main.java
+++ Mysample/src/Main.java
@@ -2,7 +2,7 @@
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Helloworld from server done ");
+		System.out.println("Helloworld from server done ,again in server ");
 	}
 
 }
